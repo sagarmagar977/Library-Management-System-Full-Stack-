536ea0dcc108be15ae8aaa77068120c3b266b981
SAGAR MAGAR <145284264+sagarmagar977@users.noreply.github.com>
2026-02-19 03:31:41 +0545

    Refactor URL patterns for consistency and clarity

--- librarymanagement/urls.py
+++ librarymanagement/urls.py
@@ -24,34 +24,28 @@ from django.contrib.auth.views import LoginView,LogoutView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('django.contrib.auth.urls') ),
-    
-    path('', views.home_view, name='home'),
+    path('', views.home_view),
 
-    path('adminclick/', views.adminclick_view),
-    path('studentclick/', views.studentclick_view),
+    path('adminclick', views.adminclick_view),
+    path('studentclick', views.studentclick_view),
 
 
-    path('adminsignup/', views.adminsignup_view),
-    path('studentsignup/', views.studentsignup_view),
-    path('adminlogin/', LoginView.as_view(template_name='library/adminlogin.html')),
-    path('studentlogin/', LoginView.as_view(template_name='library/studentlogin.html')),
-    path('logout/', views.logout_view, name='logout'),
-    # path('logout/', logout_View.as_view(template_name='library/index.html'), name='logout'),
+    path('adminsignup', views.adminsignup_view),
+    path('studentsignup', views.studentsignup_view),
+    path('adminlogin', LoginView.as_view(template_name='library/adminlogin.html')),
+    path('studentlogin', LoginView.as_view(template_name='library/studentlogin.html')),
 
-    path('afterlogin/', views.afterlogin_view),
+    path('logout', LogoutView.as_view(template_name='library/index.html')),
+    path('afterlogin', views.afterlogin_view),
 
-    path('addbook/', views.addbook_view),
-    path('viewbook/', views.viewbook_view,name='viewbook'),
-    path('issuebook/', views.issuebook_view),
-    path('viewissuedbook/', views.viewissuedbook_view),
-    path('viewstudent/', views.viewstudent_view),
-    path('viewissuedbookbystudent/', views.viewissuedbookbystudent),
+    path('addbook', views.addbook_view),
+    path('viewbook', views.viewbook_view),
+    path('issuebook', views.issuebook_view),
+    path('viewissuedbook', views.viewissuedbook_view),
+    path('viewstudent', views.viewstudent_view),
+    path('viewissuedbookbystudent', views.viewissuedbookbystudent),
 
-    path('aboutus/', views.aboutus_view),
-    path('contactus/', views.contactus_view),
+    path('aboutus', views.aboutus_view),
+    path('contactus', views.contactus_view),
 
 ]
-
-
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-urlpatterns += staticfiles_urlpatterns()
